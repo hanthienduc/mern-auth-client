abda2bce66a61e2abf2053b9215d2b1af908fba7
hanthienduc <hanthienduc.96@gmail.com>
2022-08-27 17:42:29 +0900

    add auth helper logic to save user to local storage and cookie

---
+++ src/helpers/helpers.js
@@ -0,0 +1,59 @@
+import cookie from 'js-cookie'
+
+// set in cookie
+export const setCookie = (key, value) => {
+  if (window !== 'undefined') {
+    cookie.set(key, value, {
+      expires: 1
+    })
+  }
+}
+// remove from cookie
+export const removeCookie = (key) => {
+  if (window !== 'undefined') {
+    cookie.remove(key, {
+      expires: 1
+    })
+  }
+}
+// get from cookie such as stored token
+// will be useful when we need to make request to server with token
+export const getCookie = (key) => {
+  if (window !== 'undefined') {
+    return cookie.get(key)
+  }
+}
+
+// set in localStorage
+export const setLocalStorage = (key, value) => {
+  if (window !== 'undefined') {
+    localStorage.set(key, JSON.stringify(value))
+  }
+}
+// remove from localStorage
+export const removeLocalStorage = (key) => {
+  if (window !== 'undefined') {
+    localStorage.removeItem(key)
+  }
+}
+
+// authenticate user by passing data to cookie and localStorage during signIn
+export const authenticate = (data, next) => {
+  console.log('AUTHENTICATE HELPER ON SIGNIN RESPONSE')
+  setCookie('token', data.token)
+  setLocalStorage('user', data.user)
+  next()
+}
+// access user info from localStorage
+export const isAuth = () => {
+  if (window !== 'undefined') {
+    const cookieChecked = getCookie('token')
+    if (cookieChecked) {
+      if (localStorage.getItem('user')) {
+        return JSON.parse(localStorage.getItem('user'))
+      } else {
+        return false
+      }
+    }
+  }
+}
